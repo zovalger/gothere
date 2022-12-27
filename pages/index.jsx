747f27a0ba876613126a574e59262e069a141993
zovalger <zovalger@gmail.com>
2022-12-27 10:18:28 -0400

    init next

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+// import styles from '../styles/Home.module.css'
+
+export default function Home() {
+	return (
+		<div className="container">
+			<h1 className="mb-5">Lorem, ipsum dolor.</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur vel
+				id non at labore excepturi facere doloremque sed facilis iste earum,
+				voluptates aliquid eius ullam quod similique dolores nisi? Animi.
+			</p>
+		</div>
+	);
+}
