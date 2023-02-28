421a4060048ed6e16391f786d6b435340132d9c9
zovalger <zovalger@gmail.com>
2023-02-28 19:40:37 -0400

    fix: title

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -11,6 +11,7 @@ export default function Layout({ children }) {
 				<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
 				{/* <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> */}
 				
+				<title>GoThere</title>
 			</Head>
 
 			<NavBar />
